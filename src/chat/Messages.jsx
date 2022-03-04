d783df80e857698d4df5d7e268b60607c2879633
mcafi <matteo.carniglia@gmail.com>
2022-03-05 00:26:46 +0100

    messages fix

--- src/chat/Messages.jsx
+++ src/chat/Messages.jsx
@@ -7,38 +7,42 @@ import { ChatIdContext } from "../App";
 
 const messageConverter = {
   toFirestore(m) {
-    return {author: m.autor, message: m.message, chatId: m.chatId}
+    return { author: m.autor, message: m.message, chatId: m.chatId };
   },
   fromFirestore(snapshot, options) {
-    const data = snapshot.data(options)
+    const data = snapshot.data(options);
     return {
       author: data.author,
       message: data.message,
       chatId: data.chatId,
-      id: snapshot.id
-    }
-  }
-}
-
+      id: snapshot.id,
+    };
+  },
+};
 
 export default function Messages() {
+  const chatId = useContext(ChatIdContext);
 
-  const chatId = useContext(ChatIdContext)
-
-  const messagesRef = collection(firestore, "chats", chatId, "messages").withConverter(messageConverter);
-  const q = query(messagesRef, orderBy("sent"), limit(20));
+  const messagesRef = collection(
+    firestore,
+    "chats",
+    chatId,
+    "messages"
+  ).withConverter(messageConverter);
+  const q = query(messagesRef, orderBy("sent", "desc"), limit(50));
 
   const [messages] = useCollectionData(q);
 
   const lastMessage = useRef(null);
 
   useEffect(() => {
-    lastMessage.current.scrollIntoView()
-  }, [messages])
+    lastMessage.current.scrollIntoView();
+  }, [messages]);
 
   return (
     <div className="bg-white h-96 w-full overflow-auto">
-      <ul>
+      <ul className="flex flex-col-reverse">
+        <li ref={lastMessage}></li>
         {messages &&
           messages.map((msg) => (
             <MessageItem
@@ -47,7 +51,6 @@ export default function Messages() {
               message={msg.message}
             />
           ))}
-          <li ref={lastMessage}></li>
       </ul>
     </div>
   );
